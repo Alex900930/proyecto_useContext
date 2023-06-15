86b40535c77f90957cee139721af97c48485aa6f
Alexander Herrera <Alex>
2023-06-15 08:48:12 -0400

    practicando el uso del hook useContext

--- src/Colores.js
+++ src/Colores.js
@@ -4,12 +4,15 @@ import {Contexto} from './contexto/Contexto';
 function Colores() {
     const {setColor}= useContext(Contexto);
   return (
+    <>
     <div className='colores'>
+    <h4>Cambie los colores: </h4>
         <div className='color' onClick={() => { setColor('#4285f4')}}></div>
         <div className='color' onClick={() => { setColor('#ea4335')}}></div>
         <div className='color' onClick={() => { setColor('#fbbc05')}}></div>
         <div className='color' onClick={() => { setColor('#34a853')}}></div>
     </div>
+    </>
   )
 }
 
